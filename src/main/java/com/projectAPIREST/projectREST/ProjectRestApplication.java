312606dde961030d25ee07011dbb169f3d964079
EduardoEbbers <cel.ebbers@gmail.com>
2021-07-05 08:47:21 -0300

    First Commit

---
+++ src/main/java/com/projectAPIREST/projectREST/ProjectRestApplication.java
@@ -0,0 +1,13 @@
+package com.projectAPIREST.projectREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRestApplication.class, args);
+	}
+
+}
